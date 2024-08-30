c8723f7bd5fc2d754266ee9de3e970f0be5fb92e
tkubanyc <tkubanyc@student.42heilbronn.de>
2024-08-30 14:47:02 +0200

    chore: Add check if str == NULL in ft_strlen and ft_strchr functions

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 16:39:03 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/03/12 18:40:25 by tkubanyc         ###   ########.fr       */
+/*   Updated: 2024/08/30 14:45:18 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strchr(const char *str, int c)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str && str[i] != '\0')
 	{
 		if (str[i] == (char)c)
 			return ((char *)str + i);
--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 16:44:59 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/03/12 11:59:33 by tkubanyc         ###   ########.fr       */
+/*   Updated: 2024/08/30 14:44:53 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strlen(const char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str && str[i] != '\0')
 	{
 		i++;
 	}
